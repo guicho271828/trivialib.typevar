4feb73f3272914701ce57bb51d850bc93e14f7a3
Masataro Asai <guicho2.71828@gmail.com>
2015-03-20 14:24:45 +0900

    working!

--- src/package.lisp
+++ src/package.lisp
@@ -5,15 +5,22 @@
 
 (in-package :cl-user)
 (defpackage trivialib.typevar
-  (:use :cl :alexandria :trivia)
+  (:use :cl :alexandria :trivia :trivia.fail)
   (:export
    #:polymorphic-object
    #:polymorphic-class
    #:polymorphic-class-hash
    #:polymorphic-class-typevars
-   #:ensure-polymorphic-class-instance))
+   #:ensure-polymorphic-class-instance
+   #:type-unify
+   #:make-constructor-form
+   #:define-constructor
+   #:default-initform
+   #:type-unify1
+   #:type-unification-error))
 (in-package :trivialib.typevar)
 
+;;; class definition
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defstruct (polymorphic-object (:include structure-object)))
 
@@ -48,21 +55,22 @@
   (ematch class
     ((polymorphic-class typevars)
      (let ((//-name (specialized-structure-name (class-name class) typevals))
-           (real-types (mapcar (lambda-match
+           (real-types (mapcar (lambda-ematch
                                  ((c2mop:slot-definition type)
                                   (reduce (lambda (type pair)
-                                            (match pair
+                                            (ematch pair
                                               ((cons var val)
                                                (subst val var type))))
                                           (mapcar #'cons typevars typevals)
                                           :initial-value type)))
                                (c2mop:class-direct-slots class))))
+       (format t "~&; Instantiating polymorphic class ~a" //-name)
        (prog1
          (make-instance 'structure-class
             :name //-name
             :direct-superclasses (list (find-class 'polymorphic-object))
             :direct-slots
-            (mapcar (lambda-match* 
+            (mapcar (lambda-ematch* 
                       (((c2mop:slot-definition name) type initform)
                        (list :name name
                              :type type
@@ -71,7 +79,7 @@
                              :initfunction nil)))
                     (c2mop:class-direct-slots class)
                     real-types initforms))
-         (mapc (lambda-match*
+         (mapc (lambda-ematch*
                  (((c2mop:slot-definition name) type)
                   (let ((accessor-name (symbolicate //-name '- name)))
                     (proclaim `(ftype (function (,//-name) ,type) ,accessor-name))
@@ -91,18 +99,181 @@
                (c2mop:class-direct-slots class)
                real-types))))))
 
-(defun ensure-polymorphic-class-instance (class typevals initforms)
-  (make-instance class :typevals typevals :initforms initforms))
+;; (defun ensure-polymorphic-class-instance (class typevals initforms)
+;;   (make-instance class :typevals typevals :initforms initforms))
+;; 
+;; (define-compiler-macro ensure-polymorphic-class-instance (&whole whole class typevals initforms)
+;;   (match* (class typevals initforms)
+;;     (((class class) (list 'quote typevals) (list 'quote initforms))
+;;      (make-instance class :typevals typevals :initforms initforms))
+;;     (((list 'quote (and sym (symbol))) (list 'quote typevals) (list 'quote initforms))
+;;      (make-instance (find-class sym) :typevals typevals :initforms initforms))
+;;     ((_ _ _)
+;;      whole)))
 
-(define-compiler-macro ensure-polymorphic-class-instance (&whole whole class typevals initforms)
-  (match* (class typevals initforms)
-    (((class class) (list 'quote typevals) (list 'quote initforms))
-     (make-instance class :typevals typevals :initforms initforms))
-    (((list 'quote (and sym (symbol))) (list 'quote typevals) (list 'quote initforms))
-     (make-instance (find-class sym) :typevals typevals :initforms initforms))
-    ((_ _ _)
-     whole)))
+(defun find-direct-slots (slot/keyword c)
+  (or (find slot/keyword (c2mop:class-direct-slots c)
+            :key #'c2mop:slot-definition-name)
+      (find slot/keyword (c2mop:class-direct-slots c)
+            :key #'c2mop:slot-definition-name
+            :test #'string=)
+      (some (lambda (c)
+              (find-direct-slots slot/keyword c))
+            (c2mop:class-direct-superclasses c))))
+
+(defmethod make-instance :after ((class polymorphic-class) &key &allow-other-keys)
+  (eval (make-constructor-form class)))
+
+(defmacro define-constructor (polymorphic-class)
+  (make-constructor-form (find-class polymorphic-class)))
+
+(defun make-constructor-form (class)
+  (let* ((slotds (c2mop:class-slots class))
+         (names (mapcar #'c2mop:slot-definition-name slotds))
+         (types (mapcar #'c2mop:slot-definition-type slotds)))
+    `(defun ,(class-name class) (&optional ,@names)
+       (make-instance (make-instance ',(class-name class) 
+                        :typevals (mapcar #'cdr
+                                          (type-unify
+                                           ',(polymorphic-class-typevars class)
+                                           ',types
+                                           (list ,@(mapcar (lambda (x) `(type-of ,x)) names))))
+                        :initforms (list ,@(mapcar (lambda (x) `(default-initform ,x)) names)))
+                     ,@(alist-plist
+                        (mapcar (lambda (name)
+                                  (cons (make-keyword name) name))
+                                names))))))
 
 
+;;; type unification
+;; 1. Any type variable unifies with any type expression, and is
+;; instantiated to that expression. A specific theory might restrict this
+;; rule with an occurs check.
+;; 
+;; 2. Two type constants unify only if they are the same type.
+;; 
+;; 3. Two type constructions unify only if they are applications of the
+;; same type constructor and all of their component types
+;; recursively unify.
+
+(define-condition type-unification-error (program-error) ())
+
+(defun type-unify (typevars templates types)
+  (ematch* (templates types)
+    ((nil nil)
+     nil)
+    (((list* tmpl tmpl*) (list* type type*))
+     (multiple-value-match (type-unify1 typevars tmpl type)
+       ((nil nil)
+        (error 'type-unification-error))
+       ((results t)
+        (labels ((rec (results typevars tmpl*)
+                   (ematch results
+                     (nil (values typevars tmpl*))
+                     ((list* (cons var val) results)
+                      (rec results
+                           (remove var typevars)
+                           (subst val var tmpl*))))))
+          (multiple-value-match (rec results typevars tmpl*)
+            ((typevars tmpl*)
+             (append results
+                     (type-unify typevars tmpl* type*))))))))))
+
+(defun type-unify1 (typevars template type)
+  (if (atom type)
+      (type-unify1-atomic typevars template type)
+      (type-unify1-compound typevars template type)))
+
+(defun type-unify1-atomic (typevars template type)
+  (ematch template
+    ((and (symbol)
+          (guard typevar (member typevar typevars)))
+     ;; template is an atomic typevar
+     (values (list (cons typevar type)) t))
+    ('* (warn "Avoid using * in type specification, it matches everything!")
+        (values nil t))
+    ((symbol)
+     ;; template is a standard atomic type
+     (if (subtypep type template)
+         (values nil t)
+         nil))
+    ((integer)
+     ;; FIXME possibly a dimension specifier of array
+     (ematch type
+       ((eq template) (values nil t))
+       ('* (values nil t))
+       (_ nil)))
+    ;; compound types
+    ((list* 'or rest)
+     (dolist (tmp rest)
+       (multiple-value-match
+           (type-unify1 typevars tmp type)
+         ((mapping t) (return-from type-unify1-atomic (values mapping t))))))
+    ((list* (and typespec (or 'and 'eql 'member)) _)
+     (error "~a is not supported in typevar!" typespec))))
+
+(defun type-unify1-compound (typevars template type)
+  (ematch type
+    ((list* (or (integer) '*) _)
+     ;; FIXME possibly a dimension specifier of array
+     (ematch template
+       ((and (symbol)
+             (guard typevar (member typevar typevars)))
+        ;; template is an atomic typevar
+        (values (list (cons typevar type)) t))
+       ('* (warn "Avoid using * in type specification, it matches everything!")
+           (values nil t))
+       ((list* _)
+        (unless (= (length template) (length type))
+          (fail))
+        (values (mappend (lambda (e1 e2)
+                           (multiple-value-match
+                               (type-unify1 typevars e1 e2)
+                             ((mapping t) mapping)
+                             ((_ nil) (return-from type-unify1-compound nil))))
+                         template type)
+                t))))
+    ((list* head elements1)
+     (ematch template
+       ((and (symbol)
+             (guard typevar (member typevar typevars)))
+        ;; template is an atomic typevar
+        (values (list (cons typevar type)) t))
+       ('* (warn "Avoid using * in type specification, it matches everything!")
+           (values nil t))
+       ((symbol)
+        ;; template is a standard atomic type.
+        ;; FIXME: string -> (array char *)
+        nil)
+       ((list* (eq head) elements2)
+        (let ((max (max (length elements1) (length elements2))))
+          (values (mappend (lambda (e1 e2)
+                             (multiple-value-match
+                                 (type-unify1 typevars e1 e2)
+                               ((mapping t) mapping)
+                               ((_ nil) (return-from type-unify1-compound nil))))
+                           (pad max '* elements2)
+                           (pad max '* elements1))
+                  t)))))))
+
+(defun pad (max thing list)
+  (append list
+          (make-list (- max (length list)) :initial-element thing)))
+
+;;; default-initform
+
+(defgeneric default-initform (x))
+(defmethod default-initform ((x number))
+  (coerce 0 (class-of x)))
+(defmethod default-initform ((x ratio))
+  ;; 1/1 is not a ratio. its devisor should be > 1.
+  ;; however we stick to zero.
+  0)
+(defmethod default-initform ((x sequence))
+  (coerce nil (class-of x)))
+(defmethod default-initform ((x standard-object))
+  (make-instance (type-of x)))
+(defmethod default-initform ((x structure-object))
+  (make-instance (type-of x)))
 
 
--- t/package.lisp
+++ t/package.lisp
@@ -34,4 +34,72 @@
   (finishes
     (defparameter *instance* (make-instance *kons/fixnum*)))
   (finishes
-    (defparameter *instance2* (make-instance *kons/fixnum* :kar 1))))
+    (defparameter *instance2* (make-instance *kons/fixnum* :kar 1)))
+
+  (finishes
+   ;; automatically instantiate kons/single-float/single-float
+   ;; it has compiler-macro
+   (defparameter *instance3* (kons 1.0 0.0))))
+
+(test type-unify1
+  (is (equal '((a . fixnum))
+             (type-unify1 '(a)
+                         'a
+                         'fixnum)))
+  
+  (is (equal '((a . fixnum))
+             (type-unify1 '(a)
+                         '(or float a)
+                         'fixnum)))
+  
+  (is (equal nil
+             (type-unify1 '(a)
+                         '(or float fixnum)
+                         'fixnum)))
+
+  (is-false
+   (nth-value 1 (type-unify1 '(a) 'float 'fixnum)))
+
+  #+nil
+  (is (equal '((a . string))
+             (type-unify1 '(a)
+                         '(and simple-array a)
+                         'simple-string)))
+
+  
+  (signals error
+    (equal '((a . string))
+           (type-unify1 '(a)
+                       '(and simple-array a)
+                       'simple-string)))
+  
+  (is (equal '((a . char))
+             (type-unify1 '(a)
+                         '(array a *)
+                         '(array char (3)))))
+
+  (is (equal '((a . char) (b . (1 *)))
+             (type-unify1 '(a b)
+                         '(array a b)
+                         '(array char (1 *)))))
+
+  (is (equal '((a . char) (b . 4))
+             (type-unify1 '(a b)
+                         '(array a (* b))
+                         '(array char (3 4)))))
+  
+  (is-false
+   (type-unify1 '(a)
+               '(array a (* 2))
+               '(array char (3 4)))))
+
+(test type-unify
+  (is (equal '((a . fixnum)) (type-unify '(a) '(a) '(fixnum))))
+  (is (equal '((a . fixnum)) (type-unify '(a) '(a a) '(fixnum fixnum))))
+  (signals type-unification-error
+    (type-unify '(a) '(a a) '(fixnum float)))
+  (signals type-unification-error
+    (type-unify '(a) '(a b) '(fixnum float)))
+  (is (equal '((a . fixnum) (b . float)) (type-unify '(a b) '(a b) '(fixnum float)))))
+
+
